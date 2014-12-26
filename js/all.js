d8c2fbe0d807664f49da362188a81826437b0dd2
liao <gonsakon@gmail.com>
2014-12-27 01:59:57 +0800

    no message

--- js/all.js
+++ js/all.js
@@ -8,7 +8,7 @@ $(function() {
         data: {
             columns: [
                 ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 50, 20, 10, 40, 15, 25]],type : 'bar'
+                ['data2', 50, 20, 10, 40, 15, 25]]
         },point: {
         show: false
     }
